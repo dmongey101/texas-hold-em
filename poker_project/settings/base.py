ef14a457a800d709ece7f6819ba3d60b63c57541
Donal Mongey <donalmongey@gmail.com>
2019-01-29 16:26:38 +0000

    Debug changed to false

--- poker_project/settings/base.py
+++ poker_project/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['fourth-stream-project-dmongey.c9users.io', 'com-dm-texas-holdem.herokuapp.com']
 
